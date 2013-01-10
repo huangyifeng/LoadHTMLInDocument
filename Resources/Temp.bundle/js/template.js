48fc4154e0b83069d3dc68e27f7724109c84d753
huangyifeng <luckyJerry85@gmail.com>
2013-01-10 18:17:21 +0800

    content

--- Resources/Temp.bundle/js/template.js
+++
@@ -1,9 +0,0 @@
-var setContent = function(contentHTML)
-{
-	$("#content_div").empty().append(contentHTML);
-//    alert(contentHTML);
-};
-
-$(document).ready(function(){
-    setContent("aaa");        
-});
